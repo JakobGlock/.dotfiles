210f437eef9a0f63468de1d05a3657cf0a5a21a4
Jakob Glock <jakob.glock@hotmail.com>
2020-02-13 23:24:48 +0000

    Adding new window to work tmux template

--- tmux-templates/Work-env.sh
+++ tmux-templates/Work-env.sh
@@ -1,21 +1,32 @@
 #!/bin/bash
 
+# Cheatsheet Window
 cd ~/dev/cheatsheets/
 tmux rename-window 'CheatSheet'
 tmux send-keys 'vim ~/dev/cheatsheets/sysadmin.md' C-m
 
+# Platform Repository
 cd ~/dev/platform/
 tmux new-window
 tmux rename-window 'Platform'
 tmux send-keys 'vim' C-m
 tmux split-window -v -p 10
 
+# Env Repository
 cd ~/dev/env/
 tmux new-window
 tmux rename-window "Env"
 tmux send-keys 'vim' C-m
 tmux split-window -v -p 10
 
+# Kubespray Repository
+cd ~/dev/kubespray/
+tmux new-window
+tmux rename-window "Kubespray"
+tmux send-keys 'vim' C-m
+tmux split-window -v -p 10
+
+# Other Window
 cd ~
 tmux new-window
 tmux rename-window 'ssh'
