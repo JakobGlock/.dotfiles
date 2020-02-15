ab428e291c1567a9df45bd80bee0f7b98c598caf
Jakob Glock <jakob.glock@hotmail.com>
2020-02-15 01:30:39 +0000

    Adding basic script to create multi-pane tmux windows

---
+++ tmux-templates/multi-panes.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Create multiple panes within a tmux window
+
+# Usage
+# -----------------------------------------------------------------------------
+# ./multi-panes.sh <num_panes> <sync: (on|off)>
+
+tmux new-window
+tmux rename-window 'Multi'
+
+for ((i=1; i<$1; i++));
+do
+tmux split-window -h
+tmux select-layout tiled
+done
+
+tmux setw synchronize-panes $2
