d7df7ec636da47a293a02297560f6e1ee494c652
Jakob Glock <jakob.glock@hotmail.com>
2020-02-16 18:28:43 +0000

    Adding split window to cheatsheets and making window splits bigger to work-env

--- tmux-templates/Work-env.sh
+++ tmux-templates/Work-env.sh
@@ -4,29 +4,30 @@
 cd ~/dev/cheatsheets/
 tmux rename-window 'CheatSheet'
 tmux send-keys 'vim ~/dev/cheatsheets/sysadmin.md' C-m
+tmux split-window -v -p 20
 
 # Platform Repository
 cd ~/dev/platform/
 tmux new-window
 tmux rename-window 'Platform'
 tmux send-keys 'vim' C-m
-tmux split-window -v -p 10
+tmux split-window -v -p 20
 
 # Env Repository
 cd ~/dev/env/
 tmux new-window
 tmux rename-window "Env"
 tmux send-keys 'vim' C-m
-tmux split-window -v -p 10
+tmux split-window -v -p 20
 
 # Kubespray Repository
 cd ~/dev/kubespray/
 tmux new-window
 tmux rename-window "Kubespray"
 tmux send-keys 'vim' C-m
-tmux split-window -v -p 10
+tmux split-window -v -p 20
 
-# Other Window
+# Other Windows
 cd ~
 tmux new-window
 tmux rename-window 'ssh'
