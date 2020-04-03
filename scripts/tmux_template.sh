24a2d731c82c7c3ff38c26515f558ab8ff7f472f
Jakob Glock <jakob.glock@hotmail.com>
2020-04-03 17:59:18 +0100

    Adding ability to supply a arguement to select template, or not...

--- scripts/tmux_template.sh
+++ scripts/tmux_template.sh
@@ -3,22 +3,29 @@
 # which you can choose to run, by entering a number and 
 # pressing enter
 
-clear
-echo -e "\nChoose a template:\n"
-
-i=0
 search_dir=~/.dotfiles/tmux-templates/*
-for filename in $search_dir;
-do
-	i=$(expr $i + 1)	
-	f=$(basename $filename)
-	echo -e "\t$i. ""${f%.*}"
-done
+input=0
 
-echo -e "\nPick a template:"
-
-read input
-clear
+if [[ $1 -eq 0 ]]
+then
+	clear
+	echo -e "\nChoose a template:\n"
+	
+	i=0
+	for filename in $search_dir;
+	do
+		i=$(expr $i + 1)	
+		f=$(basename $filename)
+		echo -e "\t$i. ""${f%.*}"
+	done
+	
+	echo -e "\nPick a template:"
+	
+	read input
+	clear
+else
+	input=$1
+fi
 
 i=0
 flag=false
