4431ce23920563804ec5285f097104eda69d806d
Jakob Glock <jakob.glock@hotmail.com>
2020-04-23 11:14:45 +0100

    Linting script with shellcheck

--- scripts/tmux_template.sh
+++ scripts/tmux_template.sh
@@ -3,7 +3,7 @@
 # which you can choose to run, by entering a number and 
 # pressing enter
 
-search_dir=~/.dotfiles/tmux-templates/*
+search_dir="$HOME/.dotfiles/tmux-templates/*"
 input=0
 
 if [[ $# -lt 1 ]]
@@ -14,14 +14,14 @@ then
 	i=0
 	for filename in $search_dir;
 	do
-		i=$(expr $i + 1)	
-		f=$(basename $filename)
+		i=$((i + 1))	
+		f=$(basename "$filename")
 		echo -e "\t$i. ""${f%.*}"
 	done
 	
 	echo -e "\nPick a template:"
 	
-	read input
+	read -r input
 	clear
 else
 	input=$1
@@ -31,15 +31,15 @@ i=0
 flag=false
 for filename in $search_dir;
 do
-	i=$(expr $i + 1)
-	if [ $input -eq $i ]
+	i=$((i + 1))
+	if [ "$input" -eq $i ]
 	then
 		flag=true
 		$filename && break
 	fi
 done
 
-if [ $flag == 'false' ]
+if [ "$flag" == 'false' ]
 then
 	echo "WARNING: Template does not exist, exiting"
 	sleep 2
