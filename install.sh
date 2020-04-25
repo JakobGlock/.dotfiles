7ced287e745cc3b8c4ed0372a702a6a4e1d9d46a
Jakob Glock <jakob.glock@hotmail.com>
2020-04-25 01:13:29 +0100

    Add multi environment install option to install script

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+read -p "INFO: Is this a workstation or server (w|W|s|S)" mt
+
+if [ ! "$mt" = "w" ] || [ ! "$mt" = "W" ] || [ ! "$mt" = "s" ] || [ ! "$mt" = "S" ]
+then
+	echo "ERROR: Choose a valid option, w|W for a workstation or s|S for a server"
+	exit 0
+fi
+
 # Install Vundle plugin for Vim
 if [ ! -d ~/.vim/bundle/ ]; then
 	echo "Installing Vundle plugin for Vim"
@@ -12,10 +20,19 @@ fi
 
 # Create symlinks
 echo "Installing symlinks...."
-ln -sf ~/.dotfiles/.tmux.conf ~/.tmux.conf
-ln -sf ~/.dotfiles/.vimrc ~/.vimrc
-ln -sf ~/.dotfiles/.env ~/.env
-ln -sf ~/.dotfiles/.bash_aliases ~/.bash_aliases
+
+if [ "$mt" = "w" ] || [ "$mt" = "W" ]
+then
+	# Install on workstation
+	ln -sf ~/.dotfiles/.tmux.conf ~/.tmux.conf
+	ln -sf ~/.dotfiles/.vimrc ~/.vimrc
+	ln -sf ~/.dotfiles/.env ~/.env
+	ln -sf ~/.dotfiles/.bash_aliases ~/.bash_aliases
+else
+	# Install on server
+	ln -sf ~/.dotfiles/.vimrc ~/.vimrc
+	ln -sf ~/.dotfiles/.bash_aliases ~/.bash_aliases
+fi
 
 # Install Vim plugins
 vim +PluginInstall +qall
