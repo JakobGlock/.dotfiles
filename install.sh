3bf51ddef77ee0691e1dd31ced3843e08a9167f2
Jakob Glock <jakob.glock@hotmail.com>
2020-04-25 12:33:11 +0100

    Fixing input checking, adding basic package installs and better logging

--- install.sh
+++ install.sh
@@ -1,40 +1,55 @@
 #!/bin/bash
 
-read -p "INFO: Is this a workstation or server (w|W|s|S)" mt
+echo "INFO: Installing dotfiles and basic software"
+echo ""
 
-if [ ! "$mt" = "w" ] || [ ! "$mt" = "W" ] || [ ! "$mt" = "s" ] || [ ! "$mt" = "S" ]
+# Gather info
+read -r -s -p "INFO: What is your sudo password? " spw
+echo ""
+read -r -p "INFO: Is this a workstation or server (w|W|s|S) " mt
+
+if [ ! "$mt" = "w" ] && [ ! "$mt" = "W" ] && [ ! "$mt" = "s" ] && [ ! "$mt" = "S" ]
 then
 	echo "ERROR: Choose a valid option, w|W for a workstation or s|S for a server"
 	exit 0
 fi
 
-# Install Vundle plugin for Vim
-if [ ! -d ~/.vim/bundle/ ]; then
-	echo "Installing Vundle plugin for Vim"
-	echo ""
-	git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-else
-	echo "Vundle already installed, skipping"
-	echo ""
-fi
 
-# Create symlinks
-echo "Installing symlinks...."
-
+# Create symlinks, install basic software
 if [ "$mt" = "w" ] || [ "$mt" = "W" ]
 then
 	# Install on workstation
+	echo "INFO: Installing symlinks for dotfiles"
+	echo ""
 	ln -sf ~/.dotfiles/.tmux.conf ~/.tmux.conf
 	ln -sf ~/.dotfiles/.vimrc ~/.vimrc
 	ln -sf ~/.dotfiles/.env ~/.env
 	ln -sf ~/.dotfiles/.bash_aliases ~/.bash_aliases
+	
+	echo "INFO: Installing software"
+	echo "$spw" | sudo apt install -y ansible shellcheck ncdu mc htop traceroute vim tmux
+	echo ""
 else
 	# Install on server
+	echo "INFO: Installing symlinks for dotfiles"
 	ln -sf ~/.dotfiles/.vimrc ~/.vimrc
 	ln -sf ~/.dotfiles/.bash_aliases ~/.bash_aliases
+	echo ""
+fi
+
+# Install Vundle plugin for Vim
+if [ ! -d ~/.vim/bundle/ ]; then
+	echo "INFO: Installing Vundle plugin for Vim"
+	echo ""
+	git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+else
+	echo "ERROR: Vundle already installed, skipping"
+	echo ""
 fi
 
 # Install Vim plugins
+echo "INFO: Installing Vim plugins"
 vim +PluginInstall +qall
+echo ""
 
-echo "Finished!"
+echo "INFO: Finished!"
