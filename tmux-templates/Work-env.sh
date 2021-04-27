31c25d11139672c7f804deb65c253aac75052a7f
JakobGlock <jakob.glock@hotmail.com>
2021-04-27 15:43:03 +0100

    Use -c flag for directories

--- tmux-templates/Work-env.sh
+++ tmux-templates/Work-env.sh
@@ -7,30 +7,26 @@ tmux kill-window -a
 cd ~/dev/tools/cheatsheets/
 tmux rename-window 'CheatSheet'
 tmux send-keys 'vim ~/dev/tools/cheatsheets/sysadmin.md' C-m
-tmux split-window -v -p 20
+tmux split-window -v -p 20 -c ~/dev/tools/cheatsheats
 
 # Platform Repository
-cd ~/dev/platform/
-tmux new-window
+tmux new-window -c ~/dev/platform
 tmux rename-window 'Platform'
 tmux send-keys 'vim' C-m
-tmux split-window -v -p 20
+tmux split-window -v -p 20 -c ~/dev/platform
 
 # Env Repository
-cd ~/dev/env/
-tmux new-window
+tmux new-window -c ~/dev/env
 tmux rename-window "Env"
 tmux send-keys 'vim' C-m
-tmux split-window -v -p 20
+tmux split-window -v -p 20 -c ~/dev/env
 
 # Kubespray Repository
-cd ~/dev/kubespray/
-tmux new-window
+tmux new-window -c ~/dev/kubespray
 tmux rename-window "Kubespray"
 tmux send-keys 'vim' C-m
-tmux split-window -v -p 20
+tmux split-window -v -p 20 -c ~/dev/kubespray
 
 # Other Windows
-cd ~
-tmux new-window
+tmux new-window -c ~/dev
 tmux rename-window 'ssh'
