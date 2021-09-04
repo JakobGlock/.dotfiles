624e3525c1310f41f825361db3dc488b17130a9b
jakobglock <jakob.glock@hotmail.com>
2021-09-04 14:57:59 +0100

    Adding basic CPU temp monitor to wibar

--- rc.lua
+++ rc.lua
@@ -260,6 +260,8 @@ awful.screen.connect_for_each_screen(function(s)
             layout = wibox.layout.fixed.horizontal,
             wibox.widget.systray(),
 	    s.myseperator,
+	    awful.widget.watch('bash -c "sensors | grep Tdie | awk \'{print $2}\'"', 15),
+	    s.myseperator,
 	    awful.widget.watch('bash -c "nmcli d wifi | grep "CC:D4:2E:C5:15:3C" | awk -v char=% \'{print $8char}\'"', 15),
 	    s.myseperator,
 	    awful.widget.watch('bash -c "uptime | sed -n -e \'s/^.*load average: //p\' | awk \'{print $1}\' | sed \'s/,*$//g\'"', 30),
