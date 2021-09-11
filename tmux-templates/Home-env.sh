214e2d44ecb3b2d1137f8731d915666207ce3c15
jakobglock <jakob.glock@hotmail.com>
2021-09-11 14:13:12 +0100

    Updating home-env tmux template

--- tmux-templates/Home-env.sh
+++ tmux-templates/Home-env.sh
@@ -3,23 +3,16 @@
 # Clear Current Windows
 tmux kill-window -a
 
-# Cheatsheet Window
-cd ~/dev/cheatsheets/
-tmux rename-window 'CheatSheet'
-tmux send-keys 'vim ~/dev/cheatsheets/commands.md' C-m
+# Vimwiki
+tmux new-window -c ~/vimwiki
+tmux rename-window 'Vimwiki'
+tmux send-keys 'vim index.wiki' C-m
 
-# Development Window
-cd ~/dev/
-tmux new-window
-tmux rename-window "Development"
-tmux send-keys 'vim' C-m
-tmux split-window -v -p 20
+# General Windows
+tmux new-window -c ~/
+tmux send-keys 'cd dev && tmux move-window -r && clear' C-m
+tmux rename-window 'Term1'
 
-# Other Windows
-cd ~
-tmux new-window
-tmux rename-window 'ssh'
-
-cd ~
-tmux new-window
-tmux rename-window 'ssh'
+tmux new-window -c ~/
+tmux send-keys 'cd dev && tmux move-window -r && clear' C-m
+tmux rename-window 'Term2'
