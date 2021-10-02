156336df27b904746a29948e6107f9f783894666
jakobglock <jakob.glock@hotmail.com>
2021-10-03 00:16:12 +0100

    Update home tmux template
    - Add music player
    - Change start directory

--- tmux-templates/Home-env.sh
+++ tmux-templates/Home-env.sh
@@ -8,11 +8,16 @@ tmux new-window -c ~/vimwiki
 tmux rename-window 'Vimwiki'
 tmux send-keys 'vim index.wiki' C-m
 
+# Music
+tmux new-window -c ~/Music
+tmux rename-window 'Music'
+tmux send-keys 'moc' C-m
+
 # General Windows
 tmux new-window -c ~/
-tmux send-keys 'cd dev && tmux move-window -r && clear' C-m
+tmux send-keys 'cd && tmux move-window -r && clear' C-m
 tmux rename-window 'Term1'
 
 tmux new-window -c ~/
-tmux send-keys 'cd dev && tmux move-window -r && clear' C-m
+tmux send-keys 'cd && tmux move-window -r && clear' C-m
 tmux rename-window 'Term2'
